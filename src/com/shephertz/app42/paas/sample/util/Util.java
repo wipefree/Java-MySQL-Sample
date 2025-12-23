57bcd54b574dc4e1b969f12d1f2f9b0cf8cacd37
wipefree <wipefree@gmail.com>
2025-12-23 16:19:26 +0300

    Update Util.java
    change return parameters

--- src/com/shephertz/app42/paas/sample/util/Util.java
+++ src/com/shephertz/app42/paas/sample/util/Util.java
@@ -10,7 +10,7 @@ public class Util {
 
 	static Properties dbProps = new Properties();
 	static File dbFile = null;
-	static {
+	/*static {
 		try {
 			dbFile = new File("ROOT/Config.properties");
 			dbProps.load(new FileInputStream(dbFile.getAbsolutePath()));
@@ -20,42 +20,46 @@ public class Util {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	}
+	}*/
 
 	/*
 	 * This function fetches the DB Ip from Config.properties
 	 */
 	public static String getDBIp() {
-		return dbProps.getProperty("app42.paas.db.ip");
+		return "192.168.0.245";
+		//return dbProps.getProperty("app42.paas.db.ip");
 	}
 
 	/*
 	 * This function fetches the DBPassword from Config.properties
 	 */
 	public static String getDBPassword() {
-		return dbProps.getProperty("app42.paas.db.password");
+		return "root";
+		//return dbProps.getProperty("app42.paas.db.password");
 	}
 
 	/*
 	 * This function fetches Username from Config.properties
 	 */
 	public static String getDBUser() {
-		return dbProps.getProperty("app42.paas.db.username");
+		return "root";
+		//return dbProps.getProperty("app42.paas.db.username");
 	}
 	
 	/*
 	 * This function fetches the DB Name from Config.properties
 	 */
 	public static String getDBName() {
-		return dbProps.getProperty("app42.paas.db.name");
+		return "testdb";
+		//return dbProps.getProperty("app42.paas.db.name");
 	}
 
 	/*
 	 * This function fetches the DB Port from Config.properties
 	 */
 	public static int getDBPort() {
-		return new Integer(dbProps.getProperty("app42.paas.db.port"));
+		return "3306":
+		//return new Integer(dbProps.getProperty("app42.paas.db.port"));
 	}
-	
 
 }
